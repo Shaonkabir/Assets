048aaf3dc8ebf755f55792cc96d2c5fd9dd4a42a
Shaonkabir <31365575+Shaonkabir@users.noreply.github.com>
2019-06-19 00:37:58 -0700

    vs code short cut

---
+++ vsCode_shortCut.js
@@ -0,0 +1,19 @@
+/*
+
+Open Live Server : F1F1
+
+Open Terminal : Ctrl + t 
+
+Clear Terminal : Alt + delete
+
+Copy a line Up or Down : Shift + Alt + upArrow / downArrow
+
+Delete a line :Ctrl + Shift + k
+
+
+*/
+
+
+
+
+ 
